75e0c1b0ca1fbd60fba23f3afe20b774051c3641
Lior David <liorda@gmail.com>
2018-01-24 00:50:32 +0200

    initial menu work

--- managers/scenemanager.gd
+++ managers/scenemanager.gd
@@ -4,12 +4,6 @@ extends Node
 
 # the currently active scene
 var current_scene = null
-
-func _ready():
-	# discover initial scene
-	var root = get_tree().get_root()
-	current_scene = root.get_child( root.get_child_count() -1 )
-
 	
 func goto_scene(path):
 	# This function will usually be called from a signal callback,
@@ -24,12 +18,21 @@ func goto_scene(path):
 func _deferred_goto_scene(path):
 	# Immediately free the current scene,
 	# there is no risk here.
-	current_scene.free()
+	if current_scene:
+		current_scene.free()
 	# Load new scene
 	var s = null
+	if path != null:
+		s = ResourceLoader.load(path)
+	if not s:
+		# level not found - print error and do nothing
+		print("can't find scene ", path)
+		return
+		
 	# Instance the new scene
 	current_scene = s.instance()
 	# Add it to the active scene, as child of root
-	get_tree().get_root().add_child(current_scene)
+	GameManager.add_child(current_scene)
 	# optional, to make it compatible with the SceneTree.change_scene() API
-	get_tree().set_current_scene(current_scene)
+	#get_tree().set_current_scene(current_scene)
+	
