99aca42122ec163a5ca8a8151ae52f87c5b3462c
Lior David <liorda@gmail.com>
2018-01-25 00:27:58 +0200

    more stuff from warp: menu, transition

--- managers/gamemanager.gd
+++ managers/gamemanager.gd
@@ -5,12 +5,35 @@ extends Node
 # scene switching
 onready var scene_manager = preload("res://managers/scenemanager.gd").new()
 
+var current_level = 0
+
 func _ready():
 	set_process(true)
 	
 func _process(delta):
 	pass
-	
+
+func pause():
+	TransitionScene.set_layer(5) # restore initial layer
+
+func start_level_transition(next_level):
+	var title = "Get ready!"
+	var level_txt = "Level " + str(next_level)
+	TransitionScene.init(next_level, level_txt, title)
+	TransitionScene.play()
+	if current_level == 1:
+		# special care for the first level, since it can only be triggered by the menu
+		TransitionScene.set_layer(10)
+
+func finish_level_transition(next_level):
+	current_level = next_level
+	scene_manager.goto_scene("res://levels/Level" + str(next_level) + ".tscn")
+
 func start_game():
 	# todo reset score, lives, etc.
-	scene_manager.goto_scene("res://levels/Level1.tscn")
+	current_level = 0
+	TransitionScene.stop()
+	start_level_transition(current_level)
+	
+func quit_game():
+	get_tree().quit()
--- menu/menu.gd
+++ menu/menu.gd
@@ -2,8 +2,76 @@ extends CanvasLayer
 
 onready var GameManager = get_node("/root/GameManager")
 
+# buttons
+onready var start_button = get_node("VBoxContainer/StartButton")
+onready var restart_button = get_node("VBoxContainer/RestartButton")
+onready var options_button = get_node("VBoxContainer/OptionsButton")
+onready var options_fullscreen_button = get_node("VBoxContainer/OptionsFullScreenButton")
+onready var options_music_button = get_node("VBoxContainer/OptionsMusicButton")
+onready var options_sfx_button = get_node("VBoxContainer/OptionsSfxButton")
+onready var options_return_button = get_node("VBoxContainer/OptionsReturnButton")
+onready var credits_button = get_node("VBoxContainer/CreditsButton")
+onready var quit_button = get_node("VBoxContainer/QuitButton")
+
+var options_state_start = false
+var options_state_restart = false
+
 func _ready():
 	pass
 
 func _on_StartButton_pressed():
 	GameManager.start_game()
+
+func update_options_buttons():
+	pass
+#TODO
+#	options_music_button.set_text("Music Volume: " + String(GameManager.music_level))
+#	options_sfx_button.set_text("Sfx Volume: " + String(GameManager.sfx_level))
+#	if GameManager.full_screen:
+#		options_fullscreen_button.set_text("Go Window Mode")
+#	else:
+#		options_fullscreen_button.set_text("Go Full Screen")
+
+
+func _on_OptionsButton_pressed():
+	# store menu
+	options_state_start = false
+	if start_button.is_visible():
+		options_state_start = true
+	options_state_restart = false
+	if restart_button.is_visible():
+		options_state_restart = true
+	# hide menu
+	start_button.hide()
+	restart_button.hide()
+	options_button.hide()
+	credits_button.hide()
+	quit_button.hide()
+	# show options
+	options_fullscreen_button.show()
+	options_music_button.show()
+	options_sfx_button.show()
+	options_return_button.show()
+	update_options_buttons()
+	options_return_button.grab_focus()
+
+func _on_OptionsReturnButton_pressed():
+	# hide options
+	options_fullscreen_button.hide()
+	options_music_button.hide()
+	options_sfx_button.hide()
+	options_return_button.hide()
+	# restore menu
+	if options_state_start:
+		start_button.show()
+	if options_state_restart:
+		restart_button.show()
+	options_button.show()
+	if not get_tree().is_paused():
+		credits_button.show()
+	credits_button.show()
+	quit_button.show()
+	options_button.grab_focus()
+
+func _on_QuitButton_pressed():
+	GameManager.quit_game()
