f8501c151d52225ff9795f2a0ca1735709f2b5fa
Idan-G <idangal@gmail.com>
2018-02-04 01:39:02 +0200

    Added some physics tweaks for better movement

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,16 +1,16 @@
 extends RigidBody2D
 
 # starting speed
-const DEFAULT_SPEED = 320
+const DEFAULT_SPEED = 200
 # speed limit
-const MAX_SPEED = 800
-const MIN_SPEED = 280
+const MAX_SPEED = 320
+const MIN_SPEED = 200
 # speed change when pressing left or right
-const ACCELERATION = 400
+const ACCELERATION = 100
 # 'up' force when pressing 'jump'
-const JUMP_SPEED = 800
+const JUMP_SPEED = 400
 # 'up' force change to slow down the jump speed
-const JUMP_DEACCELERATION = 2000
+const JUMP_DEACCELERATION = 600
 
 enum PLAYER_STATE {
 	default = 0,
@@ -25,7 +25,7 @@ var cur_velocity = Vector2()
 func _ready():
 	set_fixed_process(true)
 	cur_velocity.x = DEFAULT_SPEED
-	set_linear_velocity(cur_velocity)
+	#set_linear_velocity(cur_velocity)
 	player_state = PLAYER_STATE.default
 	on_ground = false
 
@@ -33,7 +33,7 @@ func _ready():
 func _fixed_process(delta):
 	
 	if player_state == PLAYER_STATE.jump:
-		cur_velocity.y += delta * JUMP_DEACCELERATION
+		cur_velocity.y += delta * JUMP_DEACCELERATION	
 	else:
 		if Input.is_action_pressed("ui_right"):
 			cur_velocity.x += ACCELERATION * delta
@@ -44,12 +44,11 @@ func _fixed_process(delta):
 			cur_velocity.y = -JUMP_SPEED
 			on_ground = false
 
+
+func _integrate_forces(state):
 	# making sure the player is not over the speed limit
 	cur_velocity.x = clamp(cur_velocity.x, MIN_SPEED, MAX_SPEED)
 	cur_velocity.y = clamp(cur_velocity.y, -JUMP_SPEED, JUMP_SPEED)
-
-
-func _integrate_forces(state):
 	set_linear_velocity(cur_velocity)
 
 
