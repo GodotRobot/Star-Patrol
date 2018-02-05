7d68b3c48fa73d8e76a14c3276fa8ae13babdfce
Lior David <liorda@gmail.com>
2018-02-06 00:03:49 +0200

    some menu animations

--- menu/menu.gd
+++ menu/menu.gd
@@ -41,41 +41,84 @@ func update_options_buttons():
 #	else:
 #		options_fullscreen_button.set_text("Go Full Screen")
 
+func hide_button(b, ratio):
+	if b.is_visible() and b extends Button:
+		# hide and thus "remove" from container
+		var rect = b.get_global_rect()
+		b.hide()
+		# make copy and show in place of the original one
+		var cpy = b.duplicate()
+		cpy.set_global_pos(rect.pos)
+		add_child(cpy)
+		cpy.show()
+		# add animations and eventually removal callback
+		for i in range(0, 3):
+			cpy.add_child(Tween.new())
+		cpy.get_child(0).interpolate_callback(cpy, 1.0, "queue_free")
+		cpy.get_child(1).interpolate_method(cpy, "set_pos", rect.pos, rect.pos - Vector2(200+500*ratio,0), 1.0, Tween.TRANS_SINE, Tween.EASE_IN)
+		cpy.get_child(2).interpolate_method(cpy, "set_opacity", 1.0, 0.0, 1.0, Tween.TRANS_SINE, Tween.EASE_OUT)
+		for c in cpy.get_children():
+			c.start()
+
+func remove_children(n):
+	if n extends Button:
+		for c in n.get_children():
+			c.queue_free()
+
+func show_button(b):
+	b.show()
+#	if b.is_hidden() and b extends Button:
+#		b.show()
+#		# add animations and eventually removal callback
+#		for i in range(0, 3):
+#			b.add_child(Tween.new())
+#		#b.get_child(0).interpolate_callback(self, 1.0, "remove_children", b)
+#		b.get_child(1).interpolate_method(b, "set_pos", b.get_pos() + Vector2(200+500*1,0), b.get_pos(), 1.0, Tween.TRANS_SINE, Tween.EASE_IN)
+#		b.get_child(2).interpolate_method(b, "set_opacity", 0.0, 1.0, 1.0, Tween.TRANS_SINE, Tween.EASE_OUT)
+#		for c in b.get_children():
+#			c.start()
+
 func hide_all_buttons():
+	var visible_buttons = Array()
+	# collect
 	options_remap_keymap.hide()
 	for c in get_node("Menu").get_children():
-		if c extends Button:
-			c.hide()
+		if c extends Button and c.is_visible():
+			visible_buttons.push_back(c)
+	# hide
+	for c in range(0, visible_buttons.size()):
+		var b = visible_buttons[c]
+		hide_button(b, float(c) / visible_buttons.size())
 
 func show_start_menu():
 	hide_all_buttons()
 	if GameManager.current_state == GameManager.GAME_STATES.START_MENU:
-		start_button.show()
+		show_button(start_button)
 		start_button.grab_focus()
 	else:
-		restart_button.show()
+		show_button(restart_button)
 		restart_button.grab_focus()
-	instructions_button.show()
-	options_button.show()
+	show_button(instructions_button)
+	show_button(options_button)
 	if not get_tree().is_paused():
-		credits_button.show()
-	credits_button.show()
-	quit_button.show()
+		show_button(credits_button)
+	show_button(credits_button)
+	show_button(quit_button)
 	
 func show_options_menu():
 	hide_all_buttons()
-	options_fullscreen_button.show()
-	options_music_button.show()
-	options_sfx_button.show()
-	options_remap_button.show()
-	options_return_button.show()
+	show_button(options_fullscreen_button)
+	show_button(options_music_button)
+	show_button(options_sfx_button)
+	show_button(options_remap_button)
+	show_button(options_return_button)
 	update_options_buttons()
 	options_return_button.grab_focus()
 
 func show_options_remap_menu():
 	hide_all_buttons()
 	options_remap_keymap.show()
-	options_remap_return_button.show()
+	show_button(options_remap_return_button)
 	options_remap_return_button.grab_focus()
 	
 # button press signal handlers
