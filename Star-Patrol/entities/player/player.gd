c8d44c11c1bed4bd407ad3d7c16f6f36c9a954cc
Idan-G <idangal@gmail.com>
2018-02-09 20:06:20 +0200

    Separated wheels from player, added 3 wheels for ground collision, work in progress

--- Star-Patrol/entities/player/player.gd
+++ Star-Patrol/entities/player/player.gd
@@ -17,16 +17,14 @@ enum PLAYER_STATE {
 	jump = 1
 }
 
+onready var wheel_front = get_node("WheelFront")
+onready var wheel_center = get_node("WheelCenter")
+onready var wheel_back = get_node("WheelBack")
+
 var player_state = PLAYER_STATE.default
 var on_ground = false
 var cur_velocity = Vector2()
-var collision_test1 = Vector2(0,0)
-var collision_test2 = Vector2(0,0)
-var collision_test3 = Vector2(0,0)
 
-onready var front_wheel_ray = get_node("FrontWheelRayCast")
-onready var center_wheel_ray = get_node("CenterWheelRayCast")
-onready var back_wheel_ray = get_node("BackWheelRayCast")
 
 func _ready():
 	set_fixed_process(true)
@@ -35,11 +33,17 @@ func _ready():
 	on_ground = false
 	GameManager.current_player = self
 	
-
+	
 func _draw():
-	draw_circle(collision_test1 - get_pos(), 10.0, Color(1.0,0,0))
-	draw_circle(collision_test2 - get_pos(), 10.0, Color(0,1.0,0))
-	draw_circle(collision_test3 - get_pos(), 10.0, Color(0,0,1.0))
+	if wheel_front.is_colliding():
+		draw_circle(wheel_front.get_collision_pos() - get_pos(), 10.0, Color(1.0,0,0))
+		
+	if wheel_center.is_colliding():
+		draw_circle(wheel_center.get_collision_pos() - get_pos(), 10.0, Color(0,1.0,0))
+	
+	if wheel_back.is_colliding():
+		draw_circle(wheel_back.get_collision_pos() - get_pos(), 10.0, Color(0,0,1.0))
+	
 
 func _fixed_process(delta):
 	
@@ -59,37 +63,8 @@ func _fixed_process(delta):
 	cur_velocity.y = clamp(cur_velocity.y, -JUMP_SPEED, JUMP_SPEED)
 	set_pos(get_pos() + cur_velocity)
 	
-	
-	if front_wheel_ray.is_colliding():
-		var hit_collider = front_wheel_ray.get_collider()
-		if hit_collider extends TileMap:
-			var hit_pos = front_wheel_ray.get_collision_point()
-			collision_test1 = hit_pos
-			
-	if center_wheel_ray.is_colliding():
-		var hit_collider = center_wheel_ray.get_collider()
-		if hit_collider extends TileMap:
-			var hit_pos = center_wheel_ray.get_collision_point()
-			collision_test2 = hit_pos
-	
-	if back_wheel_ray.is_colliding():
-		var hit_collider = back_wheel_ray.get_collider()
-		if hit_collider extends TileMap:
-			var hit_pos = back_wheel_ray.get_collision_point()
-			collision_test3 = hit_pos
-
-	
 	update()
 
 
 func is_jump_allowed():
 	return player_state == PLAYER_STATE.default and on_ground
-
-
-func _on_HitCheck_body_enter( body ):
-	var groups = body.get_groups()
-	if groups.has("ground"):
-		# player touched the ground following a jump
-		on_ground = true
-		player_state = PLAYER_STATE.default
-		cur_velocity.y = 0
