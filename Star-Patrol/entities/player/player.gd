7a8a06e26b6fdb698277136dd224916e5c9aba5b
Idan-G <idangal@gmail.com>
2018-02-10 21:36:13 +0200

    Added vehicle to ground positioning based on the wheels collision points

--- Star-Patrol/entities/player/player.gd
+++ Star-Patrol/entities/player/player.gd
@@ -12,6 +12,9 @@ const JUMP_SPEED = 3
 # 'up' force change to slow down the jump speed
 const JUMP_DEACCELERATION = 4
 
+# distance of the wheel from the vehicle on the y axis
+const vehicle_wheel_offset = 20
+
 enum PLAYER_STATE {
 	default = 0,
 	jump = 1
@@ -20,9 +23,9 @@ enum PLAYER_STATE {
 onready var wheel_front = get_node("WheelFront")
 onready var wheel_center = get_node("WheelCenter")
 onready var wheel_back = get_node("WheelBack")
+onready var wheels_array = [wheel_front, wheel_center, wheel_back]
 
 var player_state = PLAYER_STATE.default
-var on_ground = false
 var cur_velocity = Vector2()
 
 
@@ -30,41 +33,64 @@ func _ready():
 	set_fixed_process(true)
 	cur_velocity.x = DEFAULT_SPEED
 	player_state = PLAYER_STATE.default
-	on_ground = false
 	GameManager.current_player = self
 	
 	
-func _draw():
-	if wheel_front.is_colliding():
-		draw_circle(wheel_front.get_collision_pos() - get_pos(), 10.0, Color(1.0,0,0))
-		
-	if wheel_center.is_colliding():
-		draw_circle(wheel_center.get_collision_pos() - get_pos(), 10.0, Color(0,1.0,0))
-	
-	if wheel_back.is_colliding():
-		draw_circle(wheel_back.get_collision_pos() - get_pos(), 10.0, Color(0,0,1.0))
-	
-
 func _fixed_process(delta):
-	
 	if player_state == PLAYER_STATE.jump:
 		cur_velocity.y += delta * JUMP_DEACCELERATION	
+		if is_touching_ground():
+			player_state = PLAYER_STATE.default
 	else:
 		if Input.is_action_pressed("ui_right"):
 			cur_velocity.x += ACCELERATION * delta
 		if Input.is_action_pressed("ui_left"):
 			cur_velocity.x -= ACCELERATION * delta
 		if Input.is_action_pressed("ui_accept") and is_jump_allowed():
-			player_state = PLAYER_STATE.jump
-			cur_velocity.y = -JUMP_SPEED
-			on_ground = false
+			jump()
 	
 	cur_velocity.x = clamp(cur_velocity.x, MIN_SPEED, MAX_SPEED)
 	cur_velocity.y = clamp(cur_velocity.y, -JUMP_SPEED, JUMP_SPEED)
-	set_pos(get_pos() + cur_velocity)
 	
-	update()
+	update_wheels_pos()
+	update_vehicle_pos()
 
 
 func is_jump_allowed():
-	return player_state == PLAYER_STATE.default and on_ground
+	return player_state == PLAYER_STATE.default
+	
+func jump():
+	player_state = PLAYER_STATE.jump
+	cur_velocity.y = -JUMP_SPEED
+	for wheel in wheels_array:
+		wheel.reset_ground_col_check()
+	
+func update_vehicle_pos():
+	set_pos(get_pos() + cur_velocity)
+	
+	if player_state == PLAYER_STATE.default:
+		var new_pos = get_pos()
+		# use the center wheel to position the vehicle above the wheels
+		new_pos.y += wheel_center.get_pos().y - vehicle_wheel_offset
+		set_pos(new_pos)
+	
+		# reset and rotate the vehicle to match the two outer wheels ground collision points
+		set_rot(0)
+		var horizontal = Vector2(100,0)
+		var player_vector = wheel_front.get_colliding_position() - wheel_back.get_colliding_position()
+		var angle = horizontal.angle_to(player_vector)
+		set_rot(angle)
+	
+func update_wheels_pos():
+	for wheel in wheels_array:
+		if wheel.is_on_ground():
+			wheel.set_pos(wheel.get_colliding_position())
+
+func is_touching_ground():
+	var ground_contact = 0
+	for wheel in wheels_array:
+		if wheel.is_on_ground():
+			ground_contact += 1
+	
+	# we assume the vehicle tocuhes the ground of all wheels do
+	return ground_contact == wheels_array.size()
--- Star-Patrol/entities/player/wheel.gd
+++ Star-Patrol/entities/player/wheel.gd
@@ -2,26 +2,38 @@ extends Area2D
 
 onready var ray_cast = get_node("RayCast2D")
 var collision_pos = Vector2()
-var is_colliding = false
+var radius = 0.0
+var on_ground = false
 
-func _draw():
-	pass
 
 func _ready():
 	set_fixed_process(true)
-	is_colliding = false
+	on_ground = false
+	radius = get_node("Sprite").get_texture().get_width() / 2.0
 	
 func _fixed_process(delta):
-	is_colliding = false
-	
 	if ray_cast.is_colliding():
 		var colliding_obj = ray_cast.get_collider()
 		if colliding_obj extends TileMap:
 			collision_pos = ray_cast.get_collision_point()
-			is_colliding = true
+			collision_pos.y -= radius # wheel radius correction
 
-func is_colliding():
-	return is_colliding
+func is_on_ground():
+	return  on_ground
+
+# return the position of the wheel when taking into account the ray cast point on the ground
+func get_colliding_position():
+	var original_pos = get_global_transform().get_origin()
+	var offset = Vector2(0, collision_pos.y - original_pos.y)
+	return get_pos() + offset
 	
-func get_collision_pos():
-	return collision_pos
+func reset_ground_col_check():
+	on_ground = false
+	
+func _on_Wheel_body_enter(body):
+	var groups = body.get_groups()
+	if groups.has("ground"):
+		# the wheel is in contact with the ground tiles
+		on_ground = true
+		
+	
