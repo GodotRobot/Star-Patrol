a10ec80d0d599b69dda69f1b6d0f877efbeafec4
Lior David <liorda@gmail.com>
2018-02-28 00:52:11 +0200

    enemy movement, still wip

--- Star-Patrol/entities/enemy/Enemy1.gd
+++ Star-Patrol/entities/enemy/Enemy1.gd
@@ -2,10 +2,11 @@ extends Area2D
 
 onready var sprite = get_node("Sprite")
 
+const SPEED = 50.0 # movement speed
+const MAX_DIST = 700.0 # follow the player for this maximum distance before leaving / kamikazing
+
 var activated = false
-const SPEED = 50.0
-var course = 0
-var course_dir
+var dir = Vector2()
 
 func disable():
 	sprite.hide()
@@ -13,16 +14,14 @@ func disable():
 	set_layer_mask(0)
 	
 func enable():
-	sprite.show()
 	set_collision_mask(1)
 	set_layer_mask(1)
-	# enter from behind
-	#var player = GameManager.current_player
-	#if player:
-	#	var cam = player.get_node("Camera2D")
-	#	if cam:
-	#		var vp = cam.get_custom_viewport()
-	#		set_pos(Vector2(player.get_pos().x, get_pos().y))
+	# enter from behind, same height (y)
+	var r = get_global_transform().o
+	var vps = get_viewport_rect().size
+	set_pos(Vector2(r.x-vps.x/2, get_pos().y))
+	dir = Vector2(SPEED * 20.0, 1.0)
+	sprite.show()
 	
 func _ready():
 	disable()
@@ -39,16 +38,19 @@ func activate():
 		activated = true
 		enable()
 
-func update_flight(delta):
-	if course == 0:
-		course = int(rand_range(10, 40))
-		course_dir = Vector2(rand_range(-1.0, 1.0), rand_range(-1.0, 1.0))
-	course = course - 1
-	var rnd = SPEED * delta * Vector2(rand_range(-1.0, 1.0), rand_range(-1.0, 1.0))
-	set_pos(get_pos() + rnd + course_dir * delta * 70.0)
-
 func _fixed_process(delta):
 	activate()
 	if !activated:
 		return
 	update_flight(delta)
+
+func update_flight(delta):
+	# do a figure of 8	
+	#var r = get_viewport_transform()
+	#var vps = get_viewport_rect().size
+	#print('r: ', r, ', vps: ', vps)
+	#if r.x < 0.2 * vps.x || r.x > 0.8 * vps.x:
+	#	dir.x = -dir.x
+	#if r.y < 0.1 * vps.y || r.y > 0.3 * vps.y:
+	#	dir.y = -dir.y
+	translate(delta * dir)
